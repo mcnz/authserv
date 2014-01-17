6cb9472ab2f5acf4d06c8c0407dc8434cdce2d01
Julian Melchert <jm@dcd.net.nz>
2014-01-17 20:01:45 +1300

    foundation

---
+++ authserv.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+import logging
+from argparse import ArgumentParser
+
+from logger import setupLogging
+
+C_BIND_IP   = ''
+C_BIND_PORT = 25565
+
+def loadConfiguration():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-ip',
+        type=str,
+        help='address to bind to',
+        default=C_BIND_IP
+    )
+    parser.add_argument(
+        '-port',
+        type=int,
+        help='port to bind to',
+        default=C_BIND_PORT
+    )
+    parser.add_argument(
+        '-key',
+        type=str,
+        help='keypair to use during authentication'
+    )
+    parser.add_argument(
+        '--verbose',
+        help='print more information',
+        action='store_true'
+    )
+    return parser.parse_args()
+
+if __name__ == '__main__':
+
+    conf = loadConfiguration()
+    setupLogging(logging.DEBUG if conf.verbose else logging.INFO)
+
+    logging.info('Loaded configuration')
---
+++ logger.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+import logging
+
+LOG_FILE        = 'authserv.log'
+LOG_FMT         = '%(asctime)s %(name)-15s %(levelname)-7s %(message)s'
+LOG_DATEFMT     = '%y-%m-%d %H:%M:%S'
+
+def setupLogging(consolelevel=logging.INFO, logfile=LOG_FILE):
+    '''
+    sets up the logger such that:
+      debug+   -> file
+      info+    -> console
+    '''
+    logging.basicConfig(
+        level       = logging.DEBUG,
+        format      = LOG_FMT,
+        datefmt     = LOG_DATEFMT,
+        filename    = logfile,
+        filemode    = 'w'
+    )
+    # log to console
+    consolelog = logging.StreamHandler()
+    consolelog.setLevel(consolelevel)
+    consolelog.setFormatter(logging.Formatter(LOG_FMT, LOG_DATEFMT))
+    logging.getLogger().addHandler(consolelog)
